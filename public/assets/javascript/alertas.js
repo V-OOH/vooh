3199395346b4cd05fa26aa57a2c2858ee91c9efc
SamaraMendoncavic <samara.mendonca@sptech.school>
2026-05-14 16:03:45 -0300

    Adicionando gráficos mocados - Alertas

--- public/assets/javascript/alertas.js
+++ public/assets/javascript/alertas.js
@@ -1,76 +1,195 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const lista = document.getElementById("lista_displays");
-  const referencia = document.getElementById("primeiro_item");
-
-  let html = "";
-
-  for (let i = 0; i <= 50; i++) {
-
-    html += `
-        <tr id="d-${i + 1}" onclick="abrirDashboard(${i + 1})">
-        <td class="data-hora">05 de abr. 2026 às 14:35:26</td>
-            <td>D${i + 1}</td>
-            <td class="tipo-container">
-                <div class="g-tipo">
-                    <span id="nivel">Crítico</span>
-                </div>
-            </td>
-            
-            <td>São Paulo - Metrô/Linha 4</td>
-            <td class="tipo-container">
-                <div class="g-tipo">
-                    <div class="tipo"></div>
-                    <span>Alto uso de RAM</span>
-                </div>
-            </td>
-        <td class="marcar">
-  <div class="marcar-container">
-    <button class="btn-marcar" onclick="marcarMenu(event, this)">
-      <svg width="32" height="31" viewBox="0 0 32 31" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <g>
-          <path
-            d="M5 0L27 0C29.7618 0 32 2.2382 32 5L32 26C32 28.7618 29.7618 31 27 31L5 31C2.2382 31 0 28.7618 0 26L0 5C0 2.2382 2.2382 0 5 0L5 0Z"
-            fill="#FFFFFF" />
-          <g transform="translate(5.5 13)">
-            <path d="M2.5 0C3.8809 0 5 1.1191 5 2.5C5 3.8809 3.8809 5 2.5 5C1.1191 5 0 3.8809 0 2.5C0 1.1191 1.1191 0 2.5 0Z" fill="#1E0B36" />
-            <path d="M2.5 0C3.8809 0 5 1.1191 5 2.5C5 3.8809 3.8809 5 2.5 5C1.1191 5 0 3.8809 0 2.5C0 1.1191 1.1191 0 2.5 0Z" fill="#1E0B36" transform="translate(8 0)" />
-            <path d="M2.5 0C3.8809 0 5 1.1191 5 2.5C5 3.8809 3.8809 5 2.5 5C1.1191 5 0 3.8809 0 2.5C0 1.1191 1.1191 0 2.5 0Z" fill="#1E0B36" transform="translate(16 0)" />
-          </g>
-        </g>
-      </svg>
-    </button>
-
-    <div class="marcar-drop">
-      <div class="item">Marcar como lida</div>
-      <div class="item">Excluir</div>
-    </div>
-  </div>
-</td>
-        `;
+// Gráfico de Linhas
+const ctx = document.getElementById('graficoLine').getContext('2d');
+const graficoLine = new Chart(ctx, {
+  type: 'line', // Tipo do gráfico
+  data: {
+    labels: ['Abr 10', 'Abr 11', 'Abr 12', 'Abr 13', 'Abr 14', 'Abr 15'],
+    datasets: [{
+      label: 'Quantidade de Alertas',
+      data: [1, 5, 5, 12, 8, 1],
+      borderColor: 'rgba(75, 192, 192, 1)', // Cor da linha
+      backgroundColor: 'rgba(75, 192, 192, 0.2)', // Cor de preenchimento
+      borderWidth: 2,
+      tension: 0.3 // Suavidade da linha
+    }, {
+      label: 'Limite',
+      data: [10, 10, 10, 10, 10, 10],
+      borderColor: 'red',
+      borderDash: [5, 5],
+      pointRadius: 0
+    }]
+  },
+  options: {
+    plugins: {
+      legend: {
+        display: false,
+      },
+      title: {
+        display: true,
+        text: 'Volume de Alertas'
+      },
+    },
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
   }
-
-  const template = document.createElement("template");
-
-  template.innerHTML = html;
-
-  referencia.after(template.content);
 });
 
-function marcarMenu(event, botao) {
-  event.stopPropagation();
+// Gráfico de Barra Dupla
+const bar_duo = document.getElementById('grafico-bar-duo').getContext('2d');
+const graficobarduo = new Chart(bar_duo, {
+  type: 'bar', // Define o tipo como barras
+  data: {
+    labels: ['Abr 10', 'Abr 11', 'Abr 12', 'Abr 13', 'Abr 14', 'Abr 15'],
+    datasets: [{
+      title: 'Volume de Alertas',
+      label: 'Atenção',
+      data: [1, 3, 2, 7, 2, 0],
+      backgroundColor: 'rgba(235, 169, 54, 0.99)', 
+      borderColor: 'rgba(235, 169, 54, 0.99)',
+      borderWidth: 1
+    },
+    {
+      label: 'crítico',
+      data: [0, 2, 3, 5, 6, 1],
+      backgroundColor: 'rgba(255, 99, 99, 0.7)', // Cor da segunda barra
+      borderColor: 'rgba(255, 99, 99, 0.7)',
+      borderWidth: 1
+    }]
+  },
+  options: {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false
+      },
+      title: {
+        display: true,
+        text: 'Distribuição por Nível'
+      },
+    },
 
-  const menu = botao.nextElementSibling;
-  const todosMenus = document.querySelectorAll(".marcar-drop");
-
-  todosMenus.forEach((item) => {
-    if (item !== menu) {
-      item.classList.remove("ativo");
+    scales: {
+      y: {
+        beginAtZero: true
+      }
     }
-  });
 
-  menu.classList.toggle("ativo");
-}
-
+  }
+});
 
 
+// Gráfico de Barras
 
+const bar = document.getElementById('grafico-bar').getContext('2d');
+const graficobar = new Chart(bar, {
+  type: 'bar', // Define o tipo como barras
+  data: {
+    labels: ['CPU', 'Disco', 'Memória  RAM', 'Rede'],
+    datasets: [{
+      title: 'Distribuição por Causa Raiz',
+      label: 'Atenção',
+      data: [10, 5, 15, 2],
+      backgroundColor: 'rgb(109, 51, 255)', 
+      borderColor: 'rgb(109, 51, 255);',
+      borderWidth: 1
+    }]
+  },
+  options: {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false
+      },
+      title: {
+        display: true,
+        text: 'Distribuição por Nível'
+      },
+    },
+
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+
+  }
+});
+
+
+// Gráfico de Roscas
+
+const rosca = document.getElementById('grafico-rosca').getContext('2d');
+const meuGrafico = new Chart(rosca, {
+    type: 'doughnut', // Define que é um gráfico de rosca
+    data: {
+        labels: ['Presencial', 'Online'],  
+        datasets: [{
+            label: 'Votos',
+            data: [40 , 60],
+            backgroundColor: [
+                'rgb(84, 145, 199)',
+                'rgb(143, 155, 176)'
+            ],
+            borderColor: [
+                'rgb(84, 145, 199)',
+                'rgb(143, 155, 176)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+        cutout: '70%', // AQUI: Define o tamanho do buraco (percentual do raio)
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: 'Modo de Intervenção'
+            }
+        }
+    }
+});
+
+// Gráfico de Barras Vertical
+
+const bar_vertical = document.getElementById('grafico-bar-vertical').getContext('2d');
+const graficobarvertical = new Chart(bar_vertical, {
+  type: 'bar', // Define o tipo como barras
+  data: {
+    labels: ['S012', 'L010', 'N010', 'OE023', 'S112', 'S011', 'N013', 'L012', 'S002', 'S001'],
+    datasets: [{
+      title: 'Distribuição por Causa Raiz',
+      label: 'Atenção',
+      data: [32, 25, 25, 19, 18, 18, 12, 8, 5, 2],
+      backgroundColor: 'rgb(109, 51, 255)', 
+      borderColor: 'rgb(109, 51, 255);',
+      borderWidth: 1
+    }]
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    indexAxis: 'y',
+    plugins: {
+      legend: {
+        display: false
+      },
+      title: {
+        display: true,
+        text: 'Principais Pontos de Atenção'
+      },
+    },
+
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+
+  }
+});
