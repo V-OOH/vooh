638fc1587a28fc0d3818f6c27a9d1014bf22be47
MarleyS439 <marleysantos439@gmail.com>
2026-05-17 12:35:44 -0300

    Correção na sidebar

--- public/assets/javascript/sidebar.js
+++ public/assets/javascript/sidebar.js
@@ -1,7 +1,21 @@
-const button = document.querySelector('.icon');
-const sidebar = document.querySelector('.sidebar');
-const logo = document.getElementById('logo');
+const button = document.querySelector(".icon");
+const sidebar = document.querySelector(".sidebar");
+const logo = document.getElementById("logo");
 
-button.addEventListener('click', () => {
-    sidebar.classList.toggle('reduced');
+// Evento de clique para reduzir a sidebar
+button.addEventListener("click", () => {
+  if (!sidebar.classList.contains("reduced")) {
+    sidebar.classList.add("reduced");
+    localStorage.setItem("sidebar", "reduced");
+  } else {
+    sidebar.classList.remove("reduced");
+    localStorage.clear();
+  }
+});
+
+// Ao carregar a DOM, verifica se o status da sidebar é 'reduced', aplicando a classe
+document.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem("sidebar") == "reduced") {
+    sidebar.classList.add("reduced");
+  }
 });
